54855d84a0afcedeb68f2e26a17b9354f824ce31
Ahmed Elshiekh <ahmedelshiekh1@gmail.com>
2023-11-03 15:35:30 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const io = require("socket.io")(3000, {
+  cors: {
+    origin: ["http://localhost:5173"],
+  },
+});
+
+io.on("connection", (socket) => {
+  socket.on("send-message", (message, room) => {
+    if (room === "") {
+      socket.broadcast.emit("recieve-message", message);
+    } else {
+      socket.to(room).emit("recieve-message", message);
+    }
+  });
+  socket.on("join-room", (room) => {
+    socket.join(room);
+  });
+});
